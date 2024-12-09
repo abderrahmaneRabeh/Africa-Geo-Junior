735b1289c7abf30078dd01de915037cefa538fd8
Abderrahmane Rabeh <rabehlife144@gmail.com>
2024-12-09 16:36:29 +0100

    #3 afficher les données provenant de la base de données dans le modèle html/css | #2 creation de function List_des_villes() pour recupérer donner au mysql

--- db/controller.php
+++ db/controller.php
@@ -1,6 +1,8 @@
 <?php
 
-include "./connection.php";
+// include "./connection.php";
+include __DIR__ . '/connection.php';
+
 
 $conx = DbConnection();
 
@@ -8,7 +10,9 @@ $conx = DbConnection();
 function List_des_villes($conx)
 {
 
-    $sql = "SELECT * FROM ville";
+    // $sql = "SELECT * FROM ville";
+    $sql = "SELECT ville.nom AS ville_nom, ville.type, pays.nom AS pays_nom FROM ville JOIN pays ON ville.id_pays = pays.id_pays";
+
     $result = $conx->query($sql);
 
     if (!$result) {
@@ -17,3 +21,32 @@ function List_des_villes($conx)
 
     return $result->num_rows > 0 ? $result : null;
 }
+
+// function displayListHtml($data)
+// {
+//     if ($data) {
+//         echo "
+//         <table border=1 style='width:100%;text-align:center'>
+//             <tr>
+//                 <th>Ville</th>
+//                 <th>Type</th>
+//                 <th>Pays</th>
+//             </tr>
+//         ";
+//         while ($row = $data->fetch_assoc()) {
+//             echo "<tr>
+//                 <td>$row[ville_nom]</td>
+//                 <td>$row[type]</td>
+//                 <td>$row[pays_nom]</td>
+//             </tr>";
+//         }
+//         echo "</table>";
+//     }
+// }
+
+$data = List_des_villes($conx);
+
+
+
+
+// displayListHtml($data);
