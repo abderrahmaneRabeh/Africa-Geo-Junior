38ed9885e1160ebb640419def4ab97138ec4a2b0
Abderrahmane Rabeh <rabehlife144@gmail.com>
2024-12-09 17:11:57 +0100

    #2 #9 #6 ajouter un formulaire de l'ajoutement d'un nouvelle ville | crée un function qui retuen l'affichage de donnée recupée dans html

--- db/controller.php
+++ db/controller.php
@@ -21,3 +21,34 @@ function List_des_villes($conx)
 }
 
 $data = List_des_villes($conx);
+
+function dispayData($item)
+{
+    echo "
+        <div class=\"col-lg-4 col-md-6 wow fadeInUp\" data-wow-delay=\"0.1s\">
+            <div class=\"room-item shadow rounded overflow-hidden\">
+                <div class=\"position-relative\">
+                    <img class=\"img-fluid\" src=\"img/room-1.jpg\" alt=\"\">
+                    " .
+        (
+            $item['type'] === 'town' ?
+            "<small class=\"position-absolute start-0 top-100 translate-middle-y bg-success text-white rounded py-1 px-3 ms-4\">" . $item['type'] . "</small>"
+            :
+            "<small class=\"position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4\">" . $item['type'] . "</small>"
+        ) .
+        "
+            </div>
+            <div class=\"p-4 mt-2\">
+                <div class=\"d-flex justify-content-between mb-3\">
+                    <h5 class=\"mb-0\">" . $item['ville_nom'] . "</h5>
+                </div>
+                <p class=\"text-body mb-3\">" . $item['pays_nom'] . "</p>
+                <div class=\"d-flex justify-content-between\">
+                    <a class=\"btn btn-sm btn-primary rounded py-2 px-4\" href=\"\">Update</a>
+                    <a class=\"btn btn-sm btn-dark rounded py-2 px-4\" href=\"\">Delete</a>
+                </div>
+            </div>
+        </div>
+    </div>
+";
+}
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 include ".\db\controller.php";
 // displayListHtml($data);
 
+
+
 ?>
 
 <!DOCTYPE html>
@@ -88,32 +90,22 @@ include ".\db\controller.php";
                     <?php
 
                     foreach ($data as $item) {
-                        echo "
-    <div class=\"col-lg-4 col-md-6 wow fadeInUp\" data-wow-delay=\"0.1s\">
-        <div class=\"room-item shadow rounded overflow-hidden\">
-            <div class=\"position-relative\">
-                <img class=\"img-fluid\" src=\"img/room-1.jpg\" alt=\"\">
-                " . ($item['type'] === 'town' ?
-                            "<small class=\"position-absolute start-0 top-100 translate-middle-y bg-success text-white rounded py-1 px-3 ms-4\">" . $item['type'] . "</small>" :
-                            "<small class=\"position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4\">" . $item['type'] . "</small>") . "
-            </div>
-            <div class=\"p-4 mt-2\">
-                <div class=\"d-flex justify-content-between mb-3\">
-                    <h5 class=\"mb-0\">" . $item['ville_nom'] . "</h5>
-                </div>
-                <p class=\"text-body mb-3\">" . $item['pays_nom'] . "</p>
-                <div class=\"d-flex justify-content-between\">
-                    <a class=\"btn btn-sm btn-primary rounded py-2 px-4\" href=\"\">Update</a>
-                    <a class=\"btn btn-sm btn-dark rounded py-2 px-4\" href=\"\">Delete</a>
-                </div>
-            </div>
-        </div>
-    </div>
-";
 
+                        dispayData($item);
                     }
 
                     ?>
+
+                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="room-item shadow rounded overflow-hidden ">
+
+                            <div class="p-4 mt-2 d-flex justify-content-center">
+                                <a href="./pages/form.php">
+                                    <button class="btn btn-sm btn-primary rounded py-2 px-4">Ajouter un
+                                        Ville</button></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
